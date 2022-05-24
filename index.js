2eb78c1ea16af4533ecc6a2503bbfa8c12170fbe
Mark Pavel <markpavel02@gmail.com>
2022-05-23 20:37:22 -0400

    Added Prompt Questions

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "Please enter your project's name. (Required)",
+    "Please enter your project's description. (Required)",
+    "Please enter your project's installation instructions. (Required)",
+    "Please enter your project's usage information. (Required)",
+    "Please enter your project's contribution guidelines. (Required)",
+    "Please enter your project's test instructions. (Required)",
+    "Would you like to add additional sections?"
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return;
+}
 
 // Function call to initialize app
 init();
